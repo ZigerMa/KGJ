66e47d62b7effccf96670f69d0415df170faf10b
Chen wei hung <william@randomidea.com>
2018-04-14 16:51:16 +0800

    update 遊戲邏輯

---
+++ Assets/_William/Scripts/Main.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour 
+{
+    private static bool m_servicesInited = false;
+   
+    void Start()
+    {
+        InitGame();
+    }
+
+	private void InitGame()
+    {
+        if (!m_servicesInited)
+        {
+            Services.Set<EventManager>(new EventManager());
+            Services.Set<PoliceManager>(new PoliceManager());
+            Services.Set<HentaiManager>(new HentaiManager());
+            Services.Set<ChildManager>(new ChildManager());
+
+
+            m_servicesInited = true;
+        }
+    }
+
+
+    private void TestMethod()
+    {
+
+    }
+
+}
