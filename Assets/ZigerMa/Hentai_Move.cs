22012deaa61fb7b9006310d1013b102be48d9f81
ZigerMa <zigerma@gmail.com>
2018-04-15 11:59:56 +0800

    CHANGE_MOVE
    CHANGE_MOVE

--- Assets/ZigerMa/Hentai_Move.cs
+++ Assets/ZigerMa/Hentai_Move.cs
@@ -25,12 +25,27 @@ public class Hentai_Move : MonoBehaviour
 
     public void Update()
     {   //最簡單的GetAxis位移
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement * speed);
+        //rb.AddForce(movement * speed);
     }
 
 
